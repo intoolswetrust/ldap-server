0baec0e92a000c2e14fe236b212ffacca450fab6
Josef Cacek <josef.cacek@gmail.com>
2014-06-12 14:31:16 +0200

    AuthenticateWithSearch added

---
+++ src/main/java/org/jboss/test/ldap/AuthenticateWithSearch.java
@@ -0,0 +1,122 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ldap;
+
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+/**
+ * An LDAP bind application with user search by <code>uid</code> attribute.
+ * <p>
+ * Usage:
+ * 
+ * <pre>
+ * java org.jboss.test.ldap.AuthenticateWithSearch ldap://localhost:10389 jduke theduke
+ * </pre>
+ * 
+ * </p>
+ * 
+ * @author Josef Cacek
+ */
+public class AuthenticateWithSearch {
+
+    /**
+     * The main.
+     * 
+     * @param args
+     * @throws NamingException
+     */
+    public static void main(String[] args) {
+        if (args == null || args.length != 3) {
+            System.err.println("Simple LDAP authenticator");
+            System.err.println();
+            System.err.println("Usage:");
+            System.err.println("\tjava " + AuthenticateWithSearch.class.getName() + " <ldapURL> <username> <password>");
+            System.err.println();
+            System.err.println("Example:");
+            System.err.println("\tjava -cp ldap-server.jar " + AuthenticateWithSearch.class.getName()
+                    + " ldap://localhost:10389 jduke theduke");
+            System.err.println();
+            System.err.println("Exit codes:");
+            System.err.println("\t0\tAuthentication succeeded");
+            System.err.println("\t1\tWrong parameters count");
+            System.err.println("\t2\tAuthentication failed");
+            System.exit(1);
+        }
+
+        // Initial bind with Admin account - used to search User DN
+        final Properties env = new Properties();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.PROVIDER_URL, args[0]);
+        env.put(Context.SECURITY_PRINCIPAL, "uid=admin,ou=system");
+        env.put(Context.SECURITY_CREDENTIALS, "secret");
+        int exitCode = 2;
+        LdapContext ctx = null;
+        try {
+            ctx = new InitialLdapContext(env, null);
+            final SearchControls searchControls = new SearchControls();
+            searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            final String baseDN = "dc=jboss,dc=org";
+            NamingEnumeration<?> namingEnum = ctx.search(baseDN, "(uid={0})", new Object[] { args[1] }, searchControls);
+            String userDN = null;
+            if (namingEnum.hasMore()) {
+                // We found a user with given uid in the LDAP
+                SearchResult sr = (SearchResult) namingEnum.next();
+                if (sr.isRelative()) {
+                    userDN = sr.getName() + "," + baseDN;
+                } else {
+                    userDN = sr.getName();
+                }
+                System.out.println("User found: " + userDN);
+            }
+            namingEnum.close();
+            ctx.close();
+            if (userDN != null) {
+                // try bind with user DN and user's password
+                env.put(Context.SECURITY_PRINCIPAL, userDN);
+                env.put(Context.SECURITY_CREDENTIALS, args[2]);
+                ctx = new InitialLdapContext(env, null);
+                System.out.println("User is authenticated");
+                exitCode = 0;
+            }
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } finally {
+            if (ctx != null) {
+                try {
+                    ctx.close();
+                } catch (NamingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.exit(exitCode);
+    }
+}
