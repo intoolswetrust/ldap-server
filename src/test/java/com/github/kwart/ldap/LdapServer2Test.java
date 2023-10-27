0eb56a27bce960d58e643195ee734b1dc52eae72
Josef Cacek <josef.cacek@gmail.com>
2023-10-27 22:28:29 +0200

    StartTLS support

--- src/test/java/com/github/kwart/ldap/LdapServer2Test.java
+++ src/test/java/com/github/kwart/ldap/LdapServer2Test.java
@@ -18,11 +18,11 @@
  */
 package com.github.kwart.ldap;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+
 import org.junit.Test;
 
 
